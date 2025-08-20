addff440de7d84fdeae305d3f737349c8fd8d675
Kauan Santos <kauanoliver128@gmail.com>
2025-08-19 21:40:12 -0300

    Resolução do desafio

--- Program.cs
+++ Program.cs
@@ -1,3 +1,23 @@
 ﻿using DesafioPOO.Models;
 
-// TODO: Realizar os testes com as classes Nokia e Iphone
+Iphone iphone = new Iphone("+55019999334455", "iPhone 16 - Pro Max", "000-Imei", 256);
+Nokia nokia = new Nokia("+55011992870090", "Nokia Tijolão", "111-Imei", 128);
+
+Console.WriteLine("Qual aplicativo você deseja instalar?");
+
+string nomeApp = Console.ReadLine();
+Console.WriteLine("\n");
+
+Console.WriteLine($"Smartphone iPhone:");
+
+iphone.Ligar();
+iphone.ReceberLigacao();
+iphone.InstalarAplicativo(nomeApp);
+
+Console.WriteLine("\n");
+
+Console.WriteLine($"Smartphone Nokia:");
+
+nokia.Ligar();
+nokia.ReceberLigacao();
+nokia.InstalarAplicativo(nomeApp);
